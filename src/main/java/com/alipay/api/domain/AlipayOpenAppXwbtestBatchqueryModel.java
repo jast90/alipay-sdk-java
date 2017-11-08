9659e29b4e8ccddb111e0f012b1d8a0f2b4b3ba4
zhangzhiwen <zhangzhiwen@ubox.cn>
2017-11-08 13:49:23 +0800

    Initial commit

---
+++ src/main/java/com/alipay/api/domain/AlipayOpenAppXwbtestBatchqueryModel.java
@@ -0,0 +1,29 @@
+package com.alipay.api.domain;
+
+import com.alipay.api.AlipayObject;
+import com.alipay.api.internal.mapping.ApiField;
+
+/**
+ * xwbtest单纯测试
+ *
+ * @author auto create
+ * @since 1.0, 2017-10-12 20:28:12
+ */
+public class AlipayOpenAppXwbtestBatchqueryModel extends AlipayObject {
+
+	private static final long serialVersionUID = 4895523738576869755L;
+
+	/**
+	 * 1sd
+	 */
+	@ApiField("xwb")
+	private String xwb;
+
+	public String getXwb() {
+		return this.xwb;
+	}
+	public void setXwb(String xwb) {
+		this.xwb = xwb;
+	}
+
+}
---
+++ src/test/java/com/alipay/api/AlipayTradePrecreateRequestTest.java
@@ -0,0 +1,14 @@
+package com.alipay.api;
+
+import com.alipay.api.request.AlipayTradePrecreateRequest;
+
+/**
+ * @author zhiwen
+ * @date 2017/11/8
+ */
+public class AlipayTradePrecreateRequestTest {
+
+    public void pay() {
+        AlipayRequest request = new AlipayTradePrecreateRequest();
+    }
+}
